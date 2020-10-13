1f71ffe8019a1cffd08ba052ff4a8262c09cf3d4
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-13 07:40:39 -0400

    add routes for Routines && Drills

--- .history/config/routes_20201013073708.rb
+++ .history/config/routes_20201013073708.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :routines, :drills # , only: [:index, :update], :routine
+      resources :drills # , only: [:index, :update], :routine
     end
   end
 end
