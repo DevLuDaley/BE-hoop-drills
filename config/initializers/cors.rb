e45bb31b28ce98dc256db566e5623033549765bc
devludaley <daleyluis@gmail.com>
2020-10-29 11:14:35 -0400

    retry origin fix '*'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,12 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fe-hoop-drills.herokuapp.com'
+    origins '*'
 
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
 end
-
-# 
