fa275ff2681a007fa91370a56406dd742ff56833
devludaley <daleyluis@gmail.com>
2020-10-29 10:54:31 -0400

    cors trial-fix-1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://fe-hoop-drills.herokuapp.com'
 
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
 end
