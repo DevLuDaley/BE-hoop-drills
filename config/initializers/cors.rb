a9b03b18525d8aa63e11de03ff6656d41a7fc09f
devludaley <daleyluis@gmail.com>
2020-10-30 05:17:36 -0400

    + update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hoop-drills.netlify.app/'
+    origins '*'
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
+             methods: %i[get post put patch delete options head]
   end
 end
